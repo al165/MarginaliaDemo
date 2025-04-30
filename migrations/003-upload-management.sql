41ba0b61b82295a7be0f4dc5729d12d863da6aab
arran lyon <arran.lyon@hotmail.com>
2025-04-30 17:24:56 +0200

    feat: :card_file_box: Added Uploads and XRef table

---
+++ migrations/003-upload-management.sql
@@ -0,0 +1,22 @@
+-- Up
+
+CREATE TABLE Uploads (
+    id TEXT PRIMARY KEY UNIQUE,
+    createdOn INTEGER,
+    path TEXT,
+    filename TEXT,
+    mimetype TEXT
+);
+
+CREATE TABLE Notes_Uploads_XRef (
+    noteId TEXT,
+    uploadId TEXT,
+    FOREIGN KEY (noteId) REFERENCES Notes (id) ON DELETE CASCADE,
+    FOREIGN KEY (uploadId) REFERENCES Uploads (id)
+);
+
+-- Down
+
+DROP TABLE Uploads;
+DROP TABLE Notes_Uploads_XRef;
+
