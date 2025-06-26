16fa62b247562f6c397c94581b0c73b5ba127a74
arran lyon <arran.lyon@hotmail.com>
2025-06-26 11:11:58 +0200

    feat: :sparkles: Added 'clear history' button.

--- public/js/fetchhistory.js
+++ public/js/fetchhistory.js
@@ -5,11 +5,12 @@ const historyDiv = document.getElementById("room-history");
 const history = JSON.parse(localStorage.getItem("history") || '{}');
 console.log(history);
 
-if (!history) {
+if (!history || Object.keys(history).length == 0) {
     historyDiv.remove();
 } else {
-    historyDiv.innerHTML = '<em>Your rooms:</em>';
+    historyDiv.innerHTML = '<br><em>Your rooms:</em>';
     const historyList = document.createElement('ul');
+    historyList.style.marginTop = 0;
     for (const roomId of Object.keys(history)) {
         const { url, roomName } = history[roomId];
         const listItem = document.createElement('li');
@@ -20,4 +21,11 @@ if (!history) {
         historyList.appendChild(listItem);
     }
     historyDiv.appendChild(historyList);
+    const clearButton = document.createElement('button');
+    clearButton.innerText = 'Clear history';
+    clearButton.onclick = () => {
+        localStorage.clear();
+        historyDiv.remove();
+    }
+    historyDiv.appendChild(clearButton);
 }
